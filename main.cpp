9e34e10185f2461d1f3be15e02106bfa741b857d
Roman <roman24613@gmail.com>
2023-07-28 07:31:06 +0300

    added opportunity to find file on linux

--- main.cpp
+++ main.cpp
@@ -10,7 +10,14 @@ int main() {
     std::cout << std::endl;
     std::cout << "Searching..." << std::endl;
     std::cin.get();
-    startSearching(std::filesystem::path("C:\\"), fileName);
+
+#if defined(__linux__) // Or #if __linux__
+    std::filesystem::path rootDir = std::filesystem::path("/");
+#elif _WIN32
+    std::filesystem::path rootDir = std::filesystem::path("C:\\");
+#endif
+
+    startSearching(rootDir, fileName);
     std::cout << "Press enter to close this window" << std::endl;
     std::cin.get();
 
